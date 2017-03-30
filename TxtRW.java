177b4c34a8e123d5edfd8ad824739ce498e11fa7
aldonut <aldo.barrientoscalero@sjsu.edu>
2017-03-30 02:58:05 -0700

    able to format with no run time errors
    Still need to look at online courses and take last space from course name

--- TxtRW.java
+++ TxtRW.java
@@ -1,8 +1,8 @@
 package class_schedule;
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,62 +10,108 @@ public class TxtRW
 {
 	public static void main(String[] args) throws IOException
 	{
-	      File schedule = new File("test.txt");
-	      
-	    //  FileReader fr = new FileReader(schedule);
-	      //BufferedReader br = new BufferedReader(fr);
+	      File schedule = new File("schedule.txt");
+	      File formatedSchedule = new File("formatedSchedule.txt");
+	      formatedSchedule.createNewFile();
+	      BufferedWriter br = new BufferedWriter(new FileWriter(formatedSchedule));
 	      
 	      String word = "";
 	      int count = 0;
-	      String[] words = new String[20];
+	      String[] words = new String[17];
 	      Scanner sc = new Scanner(schedule);
-	      boolean first = true;
+	      boolean firstFive = true;
+	      boolean firstTwo = true;
+	      int totalCount = 0;
 
+	      
 	        while (sc.hasNext()) 
 	        {
-	      
-	        	
-	        	if(count == 6)
+	        	//skip the "sec"
+	        	if(count == 2 && firstTwo == true)
 	        	{
-	        		if(first)
+	        		sc.next();
+	        		firstTwo = false;
+	        		continue;
+	        	}
+	        	
+	        	//if you've reached the name of class
+	        	if(count == 5)
+	        	{
+	        		if(firstFive)
 		        	{
 		        		word = "";
-		        		first = false;
+		        		firstFive = false;
 		        	}
 	        		
+	        		//if you're done with the name
 	        		if(sc.hasNextDouble())
 	        		{
 	   	        	
 	        			words[count] = word;
 	        			count++;
+	        			firstFive = true;
 	        			continue;
 	        			
 	        		}
 		        	word = word + sc.next() + " ";
 		        	continue;
+	        	}
+	        	
+	        	if(count == 10)
+	        	{
+		        	word = sc.next();
+		        	System.out.println("<" + word + ">");
+		        	System.out.println(word.substring(word.length() - 3, word.length()));
+		        	words[count] = word.substring(word.length() - 3, word.length());
+		        	count++;
+		        	words[count] = word.substring(0, word.length() - 3);
+		        	System.out.println(word.substring(0, word.length() - 3));
+		        	System.out.println(words[3]);
+
+		        	count++;
 
 	        	}
 	        	
 	        	word = sc.next();
 	        	 words[count] = word;
-				System.out.println(word);
 				count++;
-	            
+				
+				
+				if(count == 16)
+				{
+					for(int i = 0; i < 15; i++)
+					{
+						br.write(words[i] + ":");
+					}
+					
+					br.write(words[15]);
+					count = 0;
+					//System.out.println(words[10] + "  " + ++totalCount);
+					firstTwo = true;
+					br.write("\n");
+					
+					
+					
+					
+						
+						
+					
+				}
+				
+				
+	        		
 	        }
-	     // while((line = br.readLine()) != null)
+	    
+	       
 	   
 	    	   
 
 	     
 		
-		for(String current: words)
-		{
-			System.out.println(current + "/");
-		}
+		
 	      
-	     // br.close();
-	     // fr.close();
 		sc.close();
+		br.close();
 	      
 
 
